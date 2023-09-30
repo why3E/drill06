1f76cdeff93f7deefff4f141c03037b6471d8f54
wonju <jwwon00@naver.com>
2023-09-30 18:37:10 +0900

    drill 6 생성

---
+++ move_character_with_mouse.py
@@ -0,0 +1,43 @@
+from pico2d import *
+import random
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+
+TUK_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
+
+def handle_events():
+    global running
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+def draw_line():
+    
+
+
+running = True
+frame =0
+x,y = 600,500
+
+while running:
+    clear_canvas()
+
+    draw_line()
+
+    handle_events()
+
+
+close_canvas()
+
+
+
+
